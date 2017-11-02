71d2dcc6caea69f526b21148c8867f7593560935
James Dradge <bobdradge@hotmail.com>
2017-11-02 21:29:53 +0000

    MenuWork
    Added in new scenes and a menuscript

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	//Load scenes
+	public void LoadCharacterCreation() 
+	{
+		Application.LoadLevel ("CharacterCreation");
+	}
+	public void LoadDraw_Phase()
+	{
+		Application.LoadLevel ("Draw_Phase");
+	}
+	public void LoadShop()
+	{
+		Application.LoadLevel ("Shop");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
