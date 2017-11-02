883559d8ce9afd1b1a4049b27812e9438af4c6ca
iain26I <icotte200@caledonian.ac.uk>
2017-11-02 21:36:13 +0000

    TrialMerge

---
+++ Assets/Scripts/Restoration.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Restoration : MonoBehaviour {
+
+    public delegate bool Renovate(GameObject building);
+    public static event Renovate onClickCheckMoney;
+
+    Color32 transpa = new Color32(255, 255, 255, 80);
+    Color32 fullyVis = new Color32(255, 255, 255, 255);
+
+    // Use this for initialization
+    void Start () {
+        GetComponent<Image>().color = transpa;
+    }
+
+    public void Restore()
+    {
+        GetComponent<Image>().color = fullyVis;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
